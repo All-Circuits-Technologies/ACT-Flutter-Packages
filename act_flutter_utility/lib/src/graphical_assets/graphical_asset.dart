235261f594316b20d20a2f70f52e12bd59eff24a
Benoit Rolandeau <benoit.rolandeau@allcircuits.com>
2025-08-27 15:51:17 +0200

    Improve the assets element and add a color parameter
    refs #21527

--- act_flutter_utility/lib/src/graphical_assets/graphical_asset.dart
+++ act_flutter_utility/lib/src/graphical_assets/graphical_asset.dart
@@ -7,5 +7,8 @@ import 'package:flutter/material.dart';
 /// This abstract class serves as a type and an interface for graphical assets
 abstract interface class GraphicalAsset {
   /// Build a widget for the asset
-  Widget getWidget({double? width, double? height});
+  ///
+  /// The [color] parameter may be managed differently following the derived asset class. But the
+  /// main principle is to color the asset with it.
+  Widget getWidget({double? width, double? height, Color? color});
 }
