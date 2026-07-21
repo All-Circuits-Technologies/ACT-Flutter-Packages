dbf08332181e722f954867c6f9281956268fb636
Benoit Rolandeau <benoit.rolandeau@allcircuits.com>
2026-07-21 08:38:27 +0000

    feat: add QRCode correction level enum
    issue #128

--- act_qr_code/lib/act_qr_code.dart
+++ act_qr_code/lib/act_qr_code.dart
@@ -4,6 +4,8 @@
 
 library;
 
+export 'package:barcode/barcode.dart' show BarcodeQRCorrectionLevel;
+
 export 'src/ui/qr_code_widget.dart';
 export 'src/ui/reader/qr_code_bloc.dart';
 export 'src/ui/reader/qr_code_event.dart';
